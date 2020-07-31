b85547e2a6b4b66fcb89ac3dcf18c3639be091a3
Kenny Vu <kennyvu@live.ca>
2020-07-30 21:22:45 -0400

    Can fetch data from Api and render routes depending on response, but need to find way to stop the fetching...

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import "./App.css";
+import styled from "styled-components";
 
 import Sidebar from "./components/Sidebar";
 import Profile from "./components/Profile";
@@ -8,20 +9,40 @@ import Home from "./components/Home";
 import Notifications from "./components/Notifications";
 import Bookrmarks from "./components/Bookmarks";
 
+import GlobalStyle from "./GlobalStyles";
+
+import { CurrentUserContext } from "./components/CurrentUserContext";
+
 function App() {
+  const { currentUser, status, test } = React.useContext(CurrentUserContext);
+
+  console.log(test);
   return (
-    <Router>
-      <div className="App">
+    <>
+      <Router>
+        <GlobalStyle />
         <Sidebar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/profile" component={Profile} />
-          <Route path="/notifications" component={Notifications} />
-          <Route path="/bookmarks" component={Bookrmarks} />
-        </Switch>
-      </div>
-    </Router>
+        <Wrapper>
+          {status === "idle" ? (
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/notifications" component={Notifications} />
+              <Route path="/bookmarks" component={Bookrmarks} />
+              <Route path="/tweet/tweetId" />
+              <Route path="/:profileId" component={Profile} />
+            </Switch>
+          ) : (
+            status
+          )}
+        </Wrapper>
+      </Router>
+    </>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 export default App;
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -2,36 +2,87 @@ import React from "react";
 import styled from "styled-components";
 
 import { Link } from "react-router-dom";
-import { GiDuck } from "react-icons/gi";
+import { FaCat, FaHome } from "react-icons/fa";
+import { FiUser, FiBookmark, FiBell } from "react-icons/fi";
+import { COLORS } from "../constants";
+
+const { primary, secondary } = COLORS;
 
 const Sidebar = () => {
   return (
     <Sidenav>
-      <GiDuck />
-      <Link to="/">Home</Link>
-      <Link to="/profile">Profile</Link>
-      <Link to="/notifications">Notifications</Link>
-      <Link to="/bookmarks">Bookmarks</Link>
+      <FaCat />
+      <Link to="/">
+        <FaHome />
+        Home
+      </Link>
+      <Link to="/:profileId">
+        <FiUser />
+        Profile
+      </Link>
+      <Link to="/notifications">
+        <FiBell />
+        Notifications
+      </Link>
+      <Link to="/bookmarks">
+        <FiBookmark />
+        Bookmarks
+      </Link>
+      <button>Quack!</button>
     </Sidenav>
   );
 };
 
 const Sidenav = styled.div`
   height: 100%;
-  width: 160px;
-  position: fixed;
+  width: 350px;
+  position: absolute;
   top: 0;
   left: 0;
-  background-color: #7fdbda;
   color: #fff;
-  font-size: 4rem;
   padding: 0.5rem;
-  a {
+  overflow-x: hidden;
+  box-shadow: 0 0 2px grey;
+  > svg {
+    color: black;
     display: block;
+    margin: 2rem auto;
+    font-size: 5rem;
+  }
+  a {
+    display: flex;
+    justify-content: flex-start;
     text-decoration: none;
-    margin: 0.5rem;
-    color: #fff;
+    margin: 2rem 2.5rem;
+    color: black;
     font-size: 1.25rem;
+    padding: 0.5rem;
+    transition: 0.2s;
+    width: 70%;
+  }
+  a:hover {
+    transform: translate(12px, 0) scale(1.2);
+    border-radius: 12px;
+    color: ${primary};
+    background-color: ${secondary};
+  }
+  a:focus {
+    color: ${primary};
+  }
+  a svg {
+    margin-right: 1.75rem;
+  }
+  button {
+    display: block;
+    margin: 4rem auto;
+    border: none;
+    padding: 0.5rem;
+    width: 120px;
+    height: 70px;
+    font-size: 1.25rem;
+    color: #fff;
+    background-color: ${primary};
+    border-radius: 8px;
   }
 `;
 
